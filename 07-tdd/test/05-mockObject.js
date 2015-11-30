ee63ae4d7327c95528d09b1684094510f18af319
magdawi <mwimmer.mmt-b2014@fh-salzburg.ac.at>
2015-11-30 12:53:12 +0100

    added mock Object test, added .gitignore

--- 07-tdd/test/05-mockObject.js
+++ 07-tdd/test/05-mockObject.js
@@ -1,9 +1,17 @@
 var should = require('should')
 var scalar = require('../src/05-mockObject.js')
 
-describe('mockObject', function(){
+describe('mockObject (contains function Login that contains other functions (authoriser and login) and a login-counter)', function(){
     
-	it('i have no idea what a mockObject is', function(){
-		
+	it('should return that 0 users have logged in', function(){
+		var no_user = {}
+		var login = new Login(no_user);
+        expect(login.loginCount()).to.equal(0);
+	})
+
+	it('should return that 1 user has logged in', function(){
+		var no_user = {authorise: function() { return true; } }
+		var login = new Login(no_user);
+        expect(login.loginCount()).to.equal(1);
 	})
 })
